31543b6cc8f4a496d22b744fb18e3f43f43bed96
Washington Obungu <tobungu@gmail.com>
2022-07-19 15:53:22 +0000

    Performed a basic babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" Starts a basic flask app """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config(object):
+    """ Configures available languages for the app """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/", strict_slashes=False)
+def greeting() -> str:
+    """ Renders the 'templates/index.html' template """
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
