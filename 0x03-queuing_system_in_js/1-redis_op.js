f85808c8a48a7067ce9edae02c6e672ed24a21eb
Washington Obungu <tobungu@gmail.com>
2022-08-16 14:04:18 +0000

    Task 2 upload

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,25 @@
+// Adds and retrieves data from a redis server
+import { createClient } from 'redis';
+const redis = require('redis')
+
+const client = createClient();
+
+client.on('error', (err) => console.log(
+    'Redis client not connected to the server', err));
+
+client.on('connect', () => console.log(
+    'Redis client connected to the server'));
+
+const setNewSchool = (schoolName, value) => {
+    client.set(schoolName, value, redis.print);
+};
+
+const displaySchoolValue = (schoolName) => {
+    client.get(schoolName, (err, reply) => {
+	console.log(reply);
+    });
+};
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
